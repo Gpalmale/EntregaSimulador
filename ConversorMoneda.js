9bdf03562904b93a01425cbd1ab788ce94f29c61
Gpalmale <gpalmale89@gmail.com>
2023-05-25 13:23:47 -0400

    Operadores avanzados

--- ConversorMoneda.js
+++ ConversorMoneda.js
@@ -1,4 +1,4 @@
-//  variables
+/*//  variables
 let monto = 0;
 let monedaOrigen = '';
 let monedaDestino = '';
@@ -60,7 +60,7 @@ obtenerMonedas().forEach(moneda => {
 });
 
 //  click del botón
-btnConvertir.addEventListener('click', convertirMoneda);
+btnConvertir.addEventListener('click', convertirMoneda);*/
 
 /*const monedasDisponibles = ['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'CLP'];
 
@@ -106,3 +106,59 @@ monedasDisponibles.forEach(moneda => {
 
 document.getElementById('convertir').addEventListener('click', convertirMoneda);
 */
+
+
+let monto = 0;
+let monedaOrigen = '';
+let monedaDestino = '';
+
+function obtenerMonedas() {
+  return ['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'CLP'];
+}
+
+function obtenerApiUrl(monedaOrigen) {
+  return `https://api.exchangerate-api.com/v4/latest/${monedaOrigen}`;
+}
+
+const inputMonto = document.getElementById('monto');
+const selectOrigen = document.getElementById('monedaOrigen');
+const selectDestino = document.getElementById('monedaDestino');
+const btnConvertir = document.getElementById('convertir');
+const outputResultado = document.getElementById('resultado');
+
+function convertirMoneda() {
+  if (!inputMonto.value || !selectOrigen.value || !selectDestino.value) {
+    outputResultado.innerText = 'Ingrese todos los valores';
+    return;
+  }
+
+  monto = parseFloat(inputMonto.value) ?? 0;
+  monedaOrigen = selectOrigen.value;
+  monedaDestino = selectDestino.value;
+
+  fetch(obtenerApiUrl(monedaOrigen))
+    .then(response => response.json())
+    .then(data => {
+      const tasaDeCambio = data.rates?.[monedaDestino];
+      const resultado = monto * (tasaDeCambio ?? 0);
+      outputResultado.innerText = `${monto} ${monedaOrigen} = ${resultado.toFixed(2)} ${monedaDestino}`;
+    })
+    .catch(error => {
+      console.error('Error al obtener los tipos de cambio:', error);
+      outputResultado.innerText = 'Error al obtener los tipos de cambio';
+    });
+}
+
+obtenerMonedas()?.forEach(moneda => {
+  const opcionOrigen = document.createElement('option');
+  opcionOrigen.value = moneda;
+  opcionOrigen.innerText = moneda;
+  selectOrigen.appendChild(opcionOrigen);
+
+  const opcionDestino = document.createElement('option');
+  opcionDestino.value = moneda;
+  opcionDestino.innerText = moneda;
+  selectDestino.appendChild(opcionDestino);
+});
+
+btnConvertir?.addEventListener('click', convertirMoneda);
